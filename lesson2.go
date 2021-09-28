08c95f9df781576e51ae67f91862ee36122509bd
JohannWong <yuxiang.wang@jesquare.com>
2021-09-28 10:07:10 +0800

    lesson2

---
+++ lesson2.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func lesson2() {
+	num_a, num_b := 10, 10
+	if num_a > num_b {
+		fmt.Println("输入的数字小于10")
+	} else if num_a < num_b {
+		fmt.Println("输入的数字大于10")
+	} else {
+		fmt.Println("输入的数字等于10")
+	}
+}
