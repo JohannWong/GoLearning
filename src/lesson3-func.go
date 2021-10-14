5f2cfc2df9185068695eb1bfdbf15040f6a64775
JohannWong <yuxiang.wang@jesquare.com>
2021-10-14 15:22:29 +0800

    fixup lesson3

--- src/lesson3-func.go
+++ src/lesson3-func.go
@@ -2,10 +2,18 @@ package main
 
 import "fmt"
 
+var num int = 10
+var numx2, numx3 int
+
 func lesson3() {
 	a, b := 100, 200
 	result := max(a, b)
 	fmt.Println(result)
+
+	numx2, numx3 = getX2AndX3(num)
+	PrintValues()
+	numx2, numx3 = getX2AndX3_2(num)
+	PrintValues()
 }
 
 func max(num_a int, num_b int) int {
@@ -16,4 +24,18 @@ func max(num_a int, num_b int) int {
 		num_x = num_b
 	}
 	return num_x
+}
+
+func PrintValues() {
+	fmt.Printf("num = %d, 2x num = %d, 3x num = %d\n", num, numx2, numx3)
+}
+
+func getX2AndX3(input int) (int, int) {
+	return 2 * input, 3 * input
+}
+
+func getX2AndX3_2(input int) (x2 int, x3 int) {
+	x2 = 2 * input
+	x3 = 3 * input
+	return
 }
